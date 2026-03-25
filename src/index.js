2646f75d5cdd33eaa9f22d60f37f91ed0d4eb53a
rajeshm <rajeshm841997@gmail.com>
2026-03-25 23:31:15 +0530

    removed sensitive files and added gitignore

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-// app.use(cors());
+
 app.use(
   cors({
-    origin: "https://pickbaskets.shop",
+    origin: ["http://localhost:5173", "https://pickbaskets.shop"],
     credentials: true,
   }),
 );
